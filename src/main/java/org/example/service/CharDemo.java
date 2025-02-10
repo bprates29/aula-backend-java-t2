4f12e2587ef44b6557aebdb6b06daa0ce759e30b
Prof. Bernardo <bernardo.prates@prof.infnet.edu.br>
2025-02-10 20:33:22 -0300

    first commit

---
+++ src/main/java/org/example/service/CharDemo.java
@@ -0,0 +1,23 @@
+package org.example.service;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch = 'X';
+        System.out.println("O valor de ch é: " + ch);
+
+        ch++;
+        System.out.println("O valor de ch é: " + ch);
+
+        ch = 90;
+        System.out.println("O valor de ch é: " + ch);
+
+        for (int i = 0; i < 500; i++) {
+            System.out.println(i + " = " + (char)i);
+        }
+
+    }
+
+
+}
